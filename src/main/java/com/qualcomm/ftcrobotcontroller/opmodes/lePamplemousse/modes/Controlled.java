e4538f28a6ac68130286be899e04526a4a44ceb4
BuildingAtom <84liadam@gmail.com>
2015-11-14 21:38:02 -0500

    Started creating the core framework for our OpMode.

---
+++ src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/lePamplemousse/modes/Controlled.java
@@ -0,0 +1,7 @@
+package com.qualcomm.ftcrobotcontroller.opmodes.lePamplemousse.modes;
+
+/**
+ * Created by Adam Li on 11/14/2015.
+ */
+public class Controlled {
+}
---
+++ src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/lePamplemousse/vars/Aliases.java
@@ -0,0 +1,13 @@
+package com.qualcomm.ftcrobotcontroller.opmodes.lePamplemousse.vars;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by Adam Li on 11/14/2015.
+ */
+public class Aliases {
+    public static DcMotor motorFrontLeft = Core.motor[0];
+    public static DcMotor motorFrontRight = Core.motor[1];
+    public static DcMotor motorBackLeft = Core.motor[2];
+    public static DcMotor motorBackRight = Core.motor[3];
+}

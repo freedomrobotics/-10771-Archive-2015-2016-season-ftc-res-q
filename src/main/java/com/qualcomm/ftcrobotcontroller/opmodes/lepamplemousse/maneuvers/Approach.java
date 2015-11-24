a77846451bf7a09f23d0f2232f5fc55510656bbe
Joel V <84veneracionj@gmail.com>
2015-11-23 20:12:16 -0500

    Made new package and classes for maneuvers(might edit/delete them later); also added a couple TODOs

---
+++ src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/lepamplemousse/maneuvers/Approach.java
@@ -0,0 +1,16 @@
+package com.qualcomm.ftcrobotcontroller.opmodes.lepamplemousse.maneuvers;
+
+/**
+ * The methods for approaching the targets like buttons, debris, etc.
+ * Created by joelv on 11/23/2015.
+ */
+public class Approach {
+
+    Approach(){
+        //Default constructor
+    }
+    /*TODO: Consult Adam
+    to figure out how to go about the classes and methods for movements and maneuvers
+    or even whether or not to consider to start working on those methods yet
+    */
+}

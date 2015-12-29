0c5633bbd15ddaed5d99d226c7c5d30c4c0a14ff
BuildingAtom <84liadam@gmail.com>
2015-12-28 21:30:52 -0500

    Barebones test file. Pretty much nothing in it.

---
+++ src/main/java/com/qualcomm/ftcrobotcontroller/tests/FtcRobotControllerActivityTest.java
@@ -0,0 +1,21 @@
+package com.qualcomm.ftcrobotcontroller.tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.qualcomm.ftcrobotcontroller.FtcRobotControllerActivity;
+
+/**
+ * Created by Adam Li on 12/28/2015.
+ */
+public class FtcRobotControllerActivityTest extends ActivityInstrumentationTestCase2<FtcRobotControllerActivity> {
+
+    private FtcRobotControllerActivity ftcRobotControllerActivity;
+
+    public FtcRobotControllerActivityTest() {
+        super(FtcRobotControllerActivity.class);
+    }
+
+    //package com.qualcomm.robotcore.wifi;
+    //http://developer.android.com/training/activity-testing/activity-functional-testing.html
+
+}

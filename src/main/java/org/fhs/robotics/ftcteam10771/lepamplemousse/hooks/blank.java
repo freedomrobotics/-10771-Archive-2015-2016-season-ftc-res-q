66fe5517637be718e5d934652abd414625bcae90
BuildingAtom <84liadam@gmail.com>
2016-01-31 21:28:06 -0500

    Revert "Everything's a mess, saving for tomorrow because headache and sleep."
    This reverts commit 364cd0cb46d21b46748739cd0fc274137034aa7b.

--- src/main/java/org/fhs/robotics/ftcteam10771/lepamplemousse/hooks/blank.java
+++
@@ -1,7 +0,0 @@
-package org.fhs.robotics.ftcteam10771.lepamplemousse.hooks;
-
-/**
- * Created by Adam Li on 1/17/2016.
- */
-public class blank {
-}
--- src/main/jni/Application.mk
+++ src/main/jni/Application.mk
@@ -1,5 +1,2 @@
-# http://developer.android.com/ndk/guides/application_mk.html
-# defines the platform to use and the achitecture to compile to
-
 APP_ABI := armeabi-v7a
 APP_PLATFORM := android-21
